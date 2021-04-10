27478f7603fa45d46d2f834ef6aa81cfc188d3ee
Josiah Espiritu <sigh.espiritu@gmail.com>
2021-04-10 17:19:21 +0800

    hi

--- app/src/main/java/com/example/ligtasapp/MainActivity.kt
+++ app/src/main/java/com/example/ligtasapp/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.ligtasapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val second = findViewById(R.id.second) as Button
+
+        second.setOnClickListener{
+            val intent = Intent(this, MainActivity2::class.java)
+            startActivity(intent)
+        }
+
+
+
     }
 }
